1a0e41f8bf912741a70b6f8284390344cad91abd
Rahul Chaudhari <prodigy@Rahuls-Air.lan>
2021-04-09 16:38:47 -0400

    pipeline test

--- search-photos/lambda_function.py
+++ search-photos/lambda_function.py
@@ -49,7 +49,7 @@ def lambda_handler(event, context):
     print(json.dumps(query))
     r = requests.get(url, auth=awsauth, headers=headers, data=json.dumps(query))
     r_dict = json.loads(r.text)
-    print("Rahul")
+    print("Rahul logging statement")
     print(r_dict)
     result_list = r_dict["hits"]["hits"]
     image_url_list = []
